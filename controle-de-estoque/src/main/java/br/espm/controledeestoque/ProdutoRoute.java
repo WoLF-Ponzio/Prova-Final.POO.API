4bcb6672735f177efa7a474e8c54e4f324119dc6
EttoreENC <99734573+EttoreENC@users.noreply.github.com>
2023-06-17 18:23:30 -0300

    post + put maps

--- controle-de-estoque/src/main/java/br/espm/controledeestoque/ProdutoRoute.java
+++ controle-de-estoque/src/main/java/br/espm/controledeestoque/ProdutoRoute.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,16 @@ public class ProdutoRoute {
     public List<Produto> listarProdutos(){
     return produtoServices.listarProdutos();
     
+    }
+
+    @PostMapping("/cadastrar-produto")
+    public void cadastrarProduto(@RequestBody Produto produto ){
+        produtoServices.cadastrarProduto(produto);
+    }
+
+    @PutMapping("/atualizar-produto/{id}")
+        public void atualizarProduto(@RequestBody Produto produto, @PathVariable String id){
+            produtoServices.atualizarProduto(produto, id);
     }
 
 }
--- controle-de-estoque/src/main/java/br/espm/controledeestoque/ProdutoServices.java
+++ controle-de-estoque/src/main/java/br/espm/controledeestoque/ProdutoServices.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.Id;
+
 @Service
 public class ProdutoServices {
     
@@ -12,6 +14,18 @@ public class ProdutoServices {
 
     public List<Produto> listarProdutos(){
         return produtoRepository.findAll();
+    }
+
+    public void cadastrarProduto(Produto produto){
+        produtoRepository.save(produto);
+    }
+
+    public void atualizarProduto(Produto produto, String id){
+        produtoRepository.findById(id).map(produtoAntigo -> {
+            produtoAntigo.setNomeProduto(produto.getNomeProduto());
+            produtoAntigo.setQuantidadeProduto(produto.getQuantidadeProduto());
+            return produtoRepository.save(produtoAntigo);
+        });
     }
 
 }
